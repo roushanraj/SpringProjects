b8b7a3109d1d33ec825c47bedf4beee58f3d2fbf
Roushan Raj <roushanrj776@gmail.com>
2021-08-13 11:07:06 +0530

    Initial Commit

---
+++ FirstSpringBootApp/src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@RequestMapping("/")
+	public String welcome() {
+		return "Welcome to first Spring Web Application";
+	}
+}
